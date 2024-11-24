f41105bcbf312a605e4173824ce19602441850d6
Sai Avala <1852436+savala@users.noreply.github.com>
2024-11-23 22:46:45 -0800

    Added two functions. last element of list and sum items in list

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,18 @@
-(*let () = print_endline "Hello, Sai!"*)
+let () = print_endline "Hello, Sai!"
 
-let () = Printf.printf "%s\n" Practice_ocaml.En.e
-let () = Printf.printf "%s\n" Practice_ocaml.Es.v
+(*let () = Printf.printf "%s\n" Practice_ocaml.En.e*)
+
+let last_elem list =
+  match List.rev list with
+  | [] -> print_endline "Empty list"
+  | x :: _ -> print_endline (string_of_int x)
+
+let rec sum_list list = 
+  match list with
+  | [] -> 0
+  | x :: rest -> x + sum_list rest
+
+let my_list = [1; 2; 3; 4]
+let () = last_elem my_list
+let () = let result = sum_list my_list in 
+  print_endline (string_of_int result)
