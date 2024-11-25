8471cfc94a78eaaac1a9105aa951844f9b480edc
Sai Avala <1852436+savala@users.noreply.github.com>
2024-11-25 09:10:51 -0800

    nth elem in list

--- bin/main.ml
+++ bin/main.ml
@@ -10,12 +10,20 @@ let rec sum_list list =
   | [] -> 0
   | x :: rest -> x + sum_list rest
 
+let rec nth_elem n list = 
+  match n, list with
+  | _, [] -> failwith "err no more elements"
+  | 0, x :: _ -> x
+  | n, _ :: rest -> nth_elem (n-1) rest
+
 let my_list = [1; 2; 3; 4]
 
 let main () = 
   print_endline "Hello, Sai!";
   last_elem my_list;
   let result = sum_list my_list in
+    print_endline (string_of_int result);
+  let result = nth_elem 2 my_list in
     print_endline (string_of_int result)
 
 let () = main ()
